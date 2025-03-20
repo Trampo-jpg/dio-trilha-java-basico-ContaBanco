41bf991b5900ce4b3081ed8f989dd751cc4ccdf7
Trampo-jpg <contato.guilhermejuniorr@gmail.com>
2025-03-20 16:34:20 -0300

    ProjetoPronto

--- Conta-Banco/src/ContaTerminal.java
+++ Conta-Banco/src/ContaTerminal.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    
+     String nomeCliente = "";
+     int numero = 0;
+     String agencia = "";
+     double saldo = 0;
+
+     Scanner scanner = new Scanner(System.in);
+
+
+     System.out.println("Nome Cliente:");
+     nomeCliente = scanner.nextLine();
+
+     System.out.println("Agência:");
+     agencia = scanner.nextLine();
+
+     
+     System.out.println("Numero:");
+     numero = scanner.nextInt();
+
+
+     System.out.println("Saldo:");
+     saldo = scanner.nextDouble();
+
+
+
+     System.out.println(" Olá " + nomeCliente + " Obrigado por criar uma conta em nosso banco, sua agência é " + agencia + " Conta " + numero + " e seu saldo " + saldo + " já está disponível para saque. "
+     );
     }
+
+
+
 }
